02e302b8234834c05a8f9481a84bbf4724ae4348
Veikko Rytivaara <veikko.rytivaara@rescomi.com>
2015-05-01 19:08:13 +0300

    Support snapshotting.

--- arch-install.sh
+++ arch-install.sh
@@ -15,73 +15,69 @@ fi
 lsblk
 read -p "Give full path to ROOT partition: " root_part
 mkfs.btrfs -f -L rootfs $root_part
-mkdir /mnt/root
-mount $root_part /mnt/root
-cd /mnt/root
-btrfs subvolume create vol-root
-btrfs subvolume create vol-etc
-btrfs subvolume create vol-var
-cd /
-umount /mnt/root
-mount -o subvol=vol-root $root_part /mnt/root
-mkdir /mnt/root/{etc,var}
-mount -o subvol=vol-etc $root_part /mnt/root/etc
-mount -o subvol=vol-var $root_part /mnt/root/var
+
+mkdir /mnt/btrfs-root
+mount $root_part /mnt/btrfs-root
+
+mkdir /mnt/btrfs-root/__current
+mkdir /mnt/btrfs-root/__snapshot
+btrfs subvolume create /mnt/btrfs-root/__current/root
+btrfs subvolume create /mnt/btrfs-root/__current/etc
+btrfs subvolume create /mnt/btrfs-root/__current/var
+
+mkdir /mnt/btrfs-current
+mount -o subvol=__current/root $root_part /mnt/btrfs-current
+
+mkdir /mnt/btrfs-current/etc
+mount -o subvol=__current/etc $root_part /mnt/btrfs-current/etc
+
+mkdir /mnt/btrfs-current/var
+mount -o subvol=__current/var $root_part /mnt/btrfs-current/var
+
 read -p "Give full path to SWAP partition: " swap_part
 mkswap $swap_part
 swapon $swap_part
+
 read -p "Give full path to BOOT partition: " boot_part
 mkfs.ext2 $boot_part
-mkdir /mnt/root/boot
-mount $boot_part /mnt/root/boot
+mkdir /mnt/btrfs-current/boot
+mount $boot_part /mnt/btrfs-current/boot
+
 read -p "Give full path to HOME partition: " home_part
-mkdir /mnt/root/home
-mount $home_part /mnt/root/home
+mkdir /mnt/btrfs-current/home
+mount $home_part /mnt/btrfs-current/home
 
 #### install base system
-pacstrap /mnt/root base base-devel
+pacstrap /mnt/btrfs-current base base-devel
 
 #### setup fstab
-genfstab -U -p /mnt/root > /mnt/root/etc/fstab
-echo 'tmpfs /tmp tmpfs nodev,nosuid 0 0' >> /mnt/root/etc/fstab
-echo 'tmpfs /dev/shm tmpfs nodev,nosuid,noexec 0 0' >> /mnt/root/etc/fstab
+genfstab -U -p /mnt/btrfs-current                    > /mnt/btrfs-current/etc/fstab
+echo 'tmpfs /tmp tmpfs nodev,nosuid 0 0'            >> /mnt/btrfs-current/etc/fstab
+echo 'tmpfs /dev/shm tmpfs nodev,nosuid,noexec 0 0' >> /mnt/btrfs-current/etc/fstab
 
 #### configure system
-arch-chroot /mnt/root sed -i 's|#en_US.UTF-8 UTF-8|en_US.UTF-8 UTF-8|g' /etc/locale.gen
-arch-chroot /mnt/root locale-gen
-arch-chroot /mnt/root echo 'LANG=en_US.UTF-8' > /etc/locale.conf
-arch-chroot /mnt/root 'export LANG=en_US.UTF-8'
-
-arch-chroot /mnt/root echo 'KEYMAP=fi' > /etc/vconsole.conf
-arch-chroot /mnt/root echo 'FONT=Lat2-Terminus16' >> /etc/vconsole.conf
-arch-chroot /mnt/root pacman -S terminus-font
-
-arch-chroot /mnt/root ln -s /usr/share/zoneinfo/Europe/Helsinki /etc/localtime
-
-arch-chroot /mnt/root hwclock --systohc --utc
-
+arch-chroot /mnt/btrfs-current sed -i 's|#en_US.UTF-8 UTF-8|en_US.UTF-8 UTF-8|g' /etc/locale.gen
+arch-chroot /mnt/btrfs-current locale-gen
+arch-chroot /mnt/btrfs-current echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+arch-chroot /mnt/btrfs-current 'export LANG=en_US.UTF-8'
+arch-chroot /mnt/btrfs-current echo 'KEYMAP=fi' > /etc/vconsole.conf
+arch-chroot /mnt/btrfs-current echo 'FONT=Lat2-Terminus16' >> /etc/vconsole.conf
+arch-chroot /mnt/btrfs-current pacman -S terminus-font
+arch-chroot /mnt/btrfs-current ln -s /usr/share/zoneinfo/Europe/Helsinki /etc/localtime
+arch-chroot /mnt/btrfs-current hwclock --systohc --utc
 read -p "Give a hostname: " hostname
-arch-chroot /mnt/root echo "$hostname" > /etc/hostname
-
-######################### nano /etc/hosts
-
+arch-chroot /mnt/btrfs-current echo "$hostname" > /etc/hostname
 ip link
 read -p "Give network interface for DHCPCD: " interface
-arch-chroot /mnt/root systemctl enable dhcpcd@${interface}.service
-
-arch-chroot /mnt/root pacman -S btrfs-progs grub os-prober
-#### read -p "Set bootable partition. Open cfdisk by pressing ENTER..." x
-#### cfdisk
+arch-chroot /mnt/btrfs-current systemctl enable dhcpcd@${interface}.service
+arch-chroot /mnt/btrfs-current pacman -S btrfs-progs grub os-prober
+arch-chroot /mnt/btrfs-current sed -i 's|MODULES=""|MODULES="crc32c"|g' /etc/mkinitcpio.conf
+arch-chroot /mnt/btrfs-current sed -i 's| fsck"| fsck btrfs"|g' /etc/mkinitcpio.conf
+arch-chroot /mnt/btrfs-current mkinitcpio -p linux
 read -p "Give full path to BOOT __device__: " boot_dev
-arch-chroot /mnt/root grub-install --target=i386-pc --recheck --debug $boot_dev
-
-arch-chroot /mnt/root sed -i 's|MODULES=""|MODULES="crc32c"|g' /etc/mkinitcpio.conf
-arch-chroot /mnt/root sed -i 's| fsck"| fsck btrfs"|g' /etc/mkinitcpio.conf
-arch-chroot /mnt/root mkinitcpio -p linux
-
-arch-chroot /mnt/root sed -i 's|^GRUB_CMDLINE_LINUX=.*$|GRUB_CMDLINE_LINUX="init=/lib/systemd/systemd rootflags=subvol=vol-root ipv6.disable=1"|' /etc/default/grub
-arch-chroot /mnt/root grub-mkconfig -o /boot/grub/grub.cfg
-
-arch-chroot /mnt/root passwd
-umount -R /mnt/root
+arch-chroot /mnt/btrfs-current grub-install --target=i386-pc --recheck --debug $boot_dev
+arch-chroot /mnt/btrfs-current sed -i 's|^GRUB_CMDLINE_LINUX=.*$|GRUB_CMDLINE_LINUX="init=/lib/systemd/systemd ipv6.disable=1"|' /etc/default/grub
+arch-chroot /mnt/btrfs-current grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt/btrfs-current passwd
+umount -R /mnt/btrfs-root
 reboot
