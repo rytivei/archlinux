64f31955a517aec3bb57b02b6bbd7dc3bd4aee32
Veikko Rytivaara <veikko.rytivaara@rescomi.com>
2015-05-01 20:37:03 +0300

    Correct fstab and kernel params.

--- arch-install.sh
+++ arch-install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#### load finnish keymap
-localectl list-keymaps | grep fi
-loadkeys fi
-
 #### check internet connection
 ping -c3 www.google.com
 if [ $? -ne 0 ]; then
@@ -21,12 +17,12 @@ mount $root_part /mnt/btrfs-root
 
 mkdir /mnt/btrfs-root/__current
 mkdir /mnt/btrfs-root/__snapshot
-btrfs subvolume create /mnt/btrfs-root/__current/root
+btrfs subvolume create /mnt/btrfs-root/__current/ROOT
 btrfs subvolume create /mnt/btrfs-root/__current/etc
 btrfs subvolume create /mnt/btrfs-root/__current/var
 
 mkdir /mnt/btrfs-current
-mount -o subvol=__current/root $root_part /mnt/btrfs-current
+mount -o subvol=__current/ROOT $root_part /mnt/btrfs-current
 
 mkdir /mnt/btrfs-current/etc
 mount -o subvol=__current/etc $root_part /mnt/btrfs-current/etc
@@ -34,9 +30,8 @@ mount -o subvol=__current/etc $root_part /mnt/btrfs-current/etc
 mkdir /mnt/btrfs-current/var
 mount -o subvol=__current/var $root_part /mnt/btrfs-current/var
 
-read -p "Give full path to SWAP partition: " swap_part
-mkswap $swap_part
-swapon $swap_part
+mkdir /mnt/btrfs-current/var/lib
+mount --bind /mnt/btrfs-root/__current/ROOT/var/lib
 
 read -p "Give full path to BOOT partition: " boot_part
 mkfs.ext2 $boot_part
@@ -47,21 +42,27 @@ read -p "Give full path to HOME partition: " home_part
 mkdir /mnt/btrfs-current/home
 mount $home_part /mnt/btrfs-current/home
 
+read -p "Give full path to SWAP partition: " swap_part
+mkswap $swap_part
+swapon $swap_part
+
 #### install base system
-pacstrap /mnt/btrfs-current base base-devel
+pacstrap /mnt/btrfs-current base base-devel btrfs-progs grub os-prober
 
 #### setup fstab
-genfstab -U -p /mnt/btrfs-current                    > /mnt/btrfs-current/etc/fstab
-echo 'tmpfs /tmp tmpfs nodev,nosuid 0 0'            >> /mnt/btrfs-current/etc/fstab
-echo 'tmpfs /dev/shm tmpfs nodev,nosuid,noexec 0 0' >> /mnt/btrfs-current/etc/fstab
+root_part_uuid=$(ls -l /dev/disk/by-uuid | grep $root_part | awk '{print $9}')
+genfstab -U -p /mnt/btrfs-current > /mnt/btrfs-current/etc/fstab
+echo "tmpfs                                     /tmp               tmpfs    nodev,nosuid        0 0"                         >> /mnt/btrfs-current/etc/fstab
+echo "tmpfs                                     /dev/shm           tmpfs    nodev,nosuid,noexec 0 0"                         >> /mnt/btrfs-current/etc/fstab
+echo "/run/btrfs-root/__current/ROOT/var/lib    /var/lib           none     bind                0 0"                         >> /mnt/btrfs-current/etc/fstab
+echo "UUID=$root_part_uuid                      /run/btrfs-root    btrfs    rw,nodev,nosuid,noexec,relatime,space_cache 0 0" >> /mnt/btrfs-current/etc/fstab
 
 #### configure system
-arch-chroot /mnt/btrfs-current sed -i 's|#en_US.UTF-8 UTF-8|en_US.UTF-8 UTF-8|g' /etc/locale.gen
+arch-chroot /mnt/btrfs-current sed -i "s|#en_US.UTF-8 UTF-8|en_US.UTF-8 UTF-8|g" /etc/locale.gen
 arch-chroot /mnt/btrfs-current locale-gen
-arch-chroot /mnt/btrfs-current echo 'LANG=en_US.UTF-8' > /etc/locale.conf
-arch-chroot /mnt/btrfs-current 'export LANG=en_US.UTF-8'
-arch-chroot /mnt/btrfs-current echo 'KEYMAP=fi' > /etc/vconsole.conf
-arch-chroot /mnt/btrfs-current echo 'FONT=Lat2-Terminus16' >> /etc/vconsole.conf
+arch-chroot /mnt/btrfs-current echo "LANG=en_US.UTF-8" > /etc/locale.conf
+arch-chroot /mnt/btrfs-current echo "KEYMAP=fi" > /etc/vconsole.conf
+arch-chroot /mnt/btrfs-current echo "FONT=Lat2-Terminus16" >> /etc/vconsole.conf
 arch-chroot /mnt/btrfs-current pacman -S terminus-font
 arch-chroot /mnt/btrfs-current ln -s /usr/share/zoneinfo/Europe/Helsinki /etc/localtime
 arch-chroot /mnt/btrfs-current hwclock --systohc --utc
@@ -70,13 +71,12 @@ arch-chroot /mnt/btrfs-current echo "$hostname" > /etc/hostname
 ip link
 read -p "Give network interface for DHCPCD: " interface
 arch-chroot /mnt/btrfs-current systemctl enable dhcpcd@${interface}.service
-arch-chroot /mnt/btrfs-current pacman -S btrfs-progs grub os-prober
-arch-chroot /mnt/btrfs-current sed -i 's|MODULES=""|MODULES="crc32c"|g' /etc/mkinitcpio.conf
-arch-chroot /mnt/btrfs-current sed -i 's| fsck"| fsck btrfs"|g' /etc/mkinitcpio.conf
+arch-chroot /mnt/btrfs-current sed -i "s|MODULES=\"\"|MODULES=\"crc32c\"|g" /etc/mkinitcpio.conf
+arch-chroot /mnt/btrfs-current sed -i "s| fsck"| fsck btrfs"|g" /etc/mkinitcpio.conf
 arch-chroot /mnt/btrfs-current mkinitcpio -p linux
 read -p "Give full path to BOOT __device__: " boot_dev
 arch-chroot /mnt/btrfs-current grub-install --target=i386-pc --recheck --debug $boot_dev
-arch-chroot /mnt/btrfs-current sed -i 's|^GRUB_CMDLINE_LINUX=.*$|GRUB_CMDLINE_LINUX="init=/lib/systemd/systemd ipv6.disable=1"|' /etc/default/grub
+arch-chroot /mnt/btrfs-current sed -i "s|^GRUB_CMDLINE_LINUX=.*$|GRUB_CMDLINE_LINUX=\"root=$root_part_uuid rootflags=subvol=__current/ROOT init=/lib/systemd/systemd ipv6.disable=1\"|" /etc/default/grub
 arch-chroot /mnt/btrfs-current grub-mkconfig -o /boot/grub/grub.cfg
 arch-chroot /mnt/btrfs-current passwd
 umount -R /mnt/btrfs-root
