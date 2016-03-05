edd976e3cb67b21c4fe16c7fd116780a49fbeed6
Veikko Rytivaara <veikko.rytivaara@gmail.com>
2016-03-05 21:45:46 +0200

    Add encryption to EXT4 rootfs.

--- arch-install.sh
+++ arch-install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+loadkeys fi
+
+read -p "Will you use WiFi for the setup ? [yes/no]" wifi
+if [ "$wifi" = "yes" ]; then
+    wifi-menu
+fi
+
 echo "[INFO] checking internet connection ..."
 ping -c3 www.google.com
 if [ $? -ne 0 ]; then
@@ -10,8 +17,10 @@ fi
 echo "----------------------------"
 echo " This script will install Arch Linux with following configuration."
 echo ""
-echo " / is BTRFS and you can have mountpoints as subvolumes under it."
+echo " / can be BTRFS and you can have mountpoints as subvolumes under it."
 echo " All other basic mountpoints that are not subvolumes are on their own partition."
+echo " OR"
+echo " / can be EXT4 (with encryption optionally)."
 echo ""
 echo " --> Boots to KDE desktop with a user that has sudo rights."
 echo "----------------------------"
@@ -29,17 +38,24 @@ if [ "$partition" = "yes" ]; then
 fi
 
 lsblk
-echo "#### SWAP partition ####"
-read -p "Give full path to SWAP partition: " swap_part
-mkswap $swap_part
-swapon $swap_part
 
 echo "#### ROOT partition ####"
 read -p "Give full path to ROOT partition: " root_part
-read -p "Give FS type for ROOT partition (ext4 or btrfs): " rootfs_type
+read -p "Give FS type for ROOT partition (ext4 or btrfs): " root_fstype
+read -p "Do you want to encrypt the ROOT partition? [yes/no]: " root_crypt
 
-if [ "$rootfs_type" = "btrfs" ]; then
-    mounted_root_path="/mnt/btrfs-current"
+if [ "$root_crypt" = "yes" -a "$root_fstype" = "btrfs" ]; then
+    echo "[ERROR] you can't use encryption with BTRFS, exiting"
+    exit 1
+fi
+
+if [ "$root_crypt" = "yes" ]; then
+    cryptsetup -y -v luksFormat $root_part
+    cryptsetup open $root_part cryptroot
+fi
+
+if [ "$root_fstype" = "btrfs" ]; then
+    root_mountpoint="/mnt/btrfs-current"
 
     mkfs.btrfs -f -L rootfs $root_part
 
@@ -50,8 +66,8 @@ if [ "$rootfs_type" = "btrfs" ]; then
     mkdir /mnt/btrfs-root/__snapshot
 
     btrfs subvolume create /mnt/btrfs-root/__current/ROOT
-    mkdir $mounted_root_path
-    mount -o subvol=__current/ROOT $root_part $mounted_root_path
+    mkdir $root_mountpoint
+    mount -o subvol=__current/ROOT $root_part $root_mountpoint
 
     echo "#### BTRFS subvolumes ####"
     echo "Give a list of mountpoints that will be btrfs subvolumes under ROOT."
@@ -65,25 +81,40 @@ if [ "$rootfs_type" = "btrfs" ]; then
         for mountpoint in $subvolumes; do
             echo "[INFO] SUBVOLUM-ING: __${mountpoint}__"
             btrfs subvolume create /mnt/btrfs-root/__current/$mountpoint
-            mkdir $mounted_root_path/$mountpoint
-            mount -o subvol=__current/$mountpoint $root_part $mounted_root_path/$mountpoint
+            mkdir $root_mountpoint/$mountpoint
+            mount -o subvol=__current/$mountpoint $root_part $root_mountpoint/$mountpoint
             sleep 2
         done
     fi
 
-elif [ "$rootfs_type" = "ext4" ]; then
+elif [ "$root_fstype" = "ext4" ]; then
 
-    mounted_root_path="/mnt/ext4_root"
+    root_mountpoint="/mnt/ext4_root"
 
     mkfs.ext4 -L rootfs $root_part
-    mkdir $mounted_root_path
-    mount $root_part $mounted_root_path
+    mkdir $root_mountpoint
+
+    if [ "$root_crypt" = "yes" ]; then
+        mount /dev/mapper/cryptroot $root_mountpoint
+    else
+        mount $root_part $root_mountpoint
+    fi
+
+    if [ "$root_crypt" = "yes" ]; then
+        umount $root_mountpoint
+        cryptsetup close cryptroot
+        cryptsetup open $root_part cryptroot
+        mount /dev/mapper/cryptroot $root_mountpoint
+    fi
+
 else
     echo "You chose something else than 'ext4' or 'btrfs', exiting ..."
     exit 1
 fi
 
-if [ "$rootfs_type" = "btrfs" ]; then
+if [ "$root_crypt" = "yes" ]; then
+    list_of_mountpoints="boot"
+elif [ "$root_fstype" = "btrfs" ]; then
     list_of_mountpoints="boot home $subvolumes"
 else
     list_of_mountpoints="boot home"
@@ -102,80 +133,78 @@ for mountpoint in $mountpoint_is_partition; do
         read -p "Give full path to __${mountpoint}__ partition: " partition
         read -p "Give filesystem type to format __${mountpoint}__ partition: " fstype
         mkfs.${fstype} $partition
-        mkdir $mounted_root_path/$mountpoint
-        mount $partition $mounted_root_path/$mountpoint
+        mkdir $root_mountpoint/$mountpoint
+        mount $partition $root_mountpoint/$mountpoint
         sleep 2
     fi
 done
 
+echo "#### SWAP partition ####"
+read -p "Give full path to SWAP partition: " swap_part
+mkswap $swap_part
+swapon $swap_part
+
 #### install base system
-pacstrap -i $mounted_root_path base btrfs-progs grub sudo sed
+pacstrap -i $root_mountpoint base btrfs-progs grub sudo sed
 
 #### setup fstab
 root_part_uuid=$(ls -l /dev/disk/by-uuid | grep $(basename $root_part) | awk '{print $9}')
-genfstab -U -p $mounted_root_path > $mounted_root_path/etc/fstab
-echo "tmpfs    /tmp        tmpfs    nodev,nosuid        0 0"  >> $mounted_root_path/etc/fstab
-echo "tmpfs    /dev/shm    tmpfs    nodev,nosuid,noexec 0 0"  >> $mounted_root_path/etc/fstab
+genfstab -U -p $root_mountpoint > $root_mountpoint/etc/fstab
+echo "tmpfs    /tmp        tmpfs    nodev,nosuid        0 0"  >> $root_mountpoint/etc/fstab
+echo "tmpfs    /dev/shm    tmpfs    nodev,nosuid,noexec 0 0"  >> $root_mountpoint/etc/fstab
 
 #### set locale
-arch-chroot $mounted_root_path sed -i "s|#en_US.UTF-8 UTF-8|en_US.UTF-8 UTF-8|g" /etc/locale.gen
-arch-chroot $mounted_root_path locale-gen
-arch-chroot $mounted_root_path echo "LANG=en_US.UTF-8" > /etc/locale.conf
+arch-chroot $root_mountpoint sed -i "s|#en_US.UTF-8 UTF-8|en_US.UTF-8 UTF-8|g" /etc/locale.gen
+arch-chroot $root_mountpoint locale-gen
+arch-chroot $root_mountpoint echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
 #### set keyboard
-arch-chroot $mounted_root_path echo "KEYMAP=fi"             > /etc/vconsole.conf
-arch-chroot $mounted_root_path echo "FONT=Lat2-Terminus16" >> /etc/vconsole.conf
+arch-chroot $root_mountpoint echo "KEYMAP=fi"             > /etc/vconsole.conf
+arch-chroot $root_mountpoint echo "FONT=Lat2-Terminus16" >> /etc/vconsole.conf
 
 #### set timezone
-arch-chroot $mounted_root_path ln -s /usr/share/zoneinfo/Europe/Helsinki /etc/localtime
+arch-chroot $root_mountpoint ln -s /usr/share/zoneinfo/Europe/Helsinki /etc/localtime
 
 #### set system clock
-arch-chroot $mounted_root_path hwclock --systohc --utc
+arch-chroot $root_mountpoint hwclock --systohc --utc
 
 #### hostname
 read -p "Give a hostname: " hostname
-arch-chroot $mounted_root_path echo "$hostname" > /etc/hostname
+arch-chroot $root_mountpoint echo "$hostname" > /etc/hostname
 
 #### sudo permissions
-arch-chroot $mounted_root_path sed -i "s|# %wheel.*|%wheel ALL=(ALL) ALL|g" /etc/sudoers
+arch-chroot $root_mountpoint sed -i "s|# %wheel.*|%wheel ALL=(ALL) ALL|g" /etc/sudoers
 
 #### root password
 echo "Give a root password."
-arch-chroot $mounted_root_path passwd
+arch-chroot $root_mountpoint passwd
 
 #### create new user
 read -p "Give a username: " username
-arch-chroot $mounted_root_path useradd --create-home --groups wheel,users,uucp -s /bin/bash $username
-arch-chroot $mounted_root_path chfn $username
+arch-chroot $root_mountpoint useradd --create-home --groups wheel,users,uucp -s /bin/bash $username
+arch-chroot $root_mountpoint chfn $username
 echo "Give $username a password."
-arch-chroot $mounted_root_path passwd $username
-
-if [ "$rootfs_type" = "btrfs" ]; then
-    #### btrfs hook to initramfs
-    arch-chroot $mounted_root_path sed -i "s|MODULES=\"\"|MODULES=\"crc32c\"|g" /etc/mkinitcpio.conf
-    arch-chroot $mounted_root_path sed -i "s| fsck\"| fsck btrfs\"|g"           /etc/mkinitcpio.conf
-    arch-chroot $mounted_root_path mkinitcpio -p linux
-fi
+arch-chroot $root_mountpoint passwd $username
 
 #### install grub
 lsblk
 read -p "Give full path to BOOT __device__: " boot_dev
-arch-chroot $mounted_root_path grub-install --target=i386-pc --recheck --debug $boot_dev
-if [ "$rootfs_type" = "btrfs" ]; then
-    arch-chroot $mounted_root_path sed -i "s|^GRUB_CMDLINE_LINUX=.*$|GRUB_CMDLINE_LINUX=\"init=/lib/systemd/systemd ipv6.disable=1\"|" /etc/default/grub
+arch-chroot $root_mountpoint grub-install --target=i386-pc --recheck --debug $boot_dev
+if [ "$root_fstype" = "btrfs" ]; then
+    arch-chroot $root_mountpoint sed -i "s|^GRUB_CMDLINE_LINUX=.*$|GRUB_CMDLINE_LINUX=\"init=/lib/systemd/systemd ipv6.disable=1\"|" /etc/default/grub
 else
-    arch-chroot $mounted_root_path sed -i "s|^GRUB_CMDLINE_LINUX=.*$|GRUB_CMDLINE_LINUX=\"ipv6.disable=1\"|" /etc/default/grub
+    arch-chroot $root_mountpoint sed -i "s|^GRUB_CMDLINE_LINUX=.*$|GRUB_CMDLINE_LINUX=\"ipv6.disable=1\"|" /etc/default/grub
 fi
-arch-chroot $mounted_root_path sed -i "s|^GRUB_GFXMODE=.*|GRUB_GFXMODE=1024x768x32,auto|g"                                         /etc/default/grub
-arch-chroot $mounted_root_path grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot $root_mountpoint sed -i "s|^GRUB_GFXMODE=.*|GRUB_GFXMODE=1024x768x32,auto|g"                                         /etc/default/grub
+arch-chroot $root_mountpoint grub-mkconfig -o /boot/grub/grub.cfg
 
 #### networking
 echo "[INFO] setup dhcp networking ..."
 ip link
 read -p "Give network interface for system-networkd (dhcpcd): " interface
-arch-chroot $mounted_root_path systemctl enable dhcpcd@${interface}.service
+arch-chroot $root_mountpoint systemctl enable dhcpcd@${interface}.service
 
-cat << EOF > $mounted_root_path/etc/systemd/network/wired.network
+cat << EOF > $root_mountpoint/etc/systemd/network/wired.network
 [Match]
 Name=$interface
 
@@ -190,18 +219,31 @@ echo ""
 sleep 5
 
 #### install rest of the packages
-arch-chroot $mounted_root_path pacman -S base-devel abs htop bash-completion vim terminus-font git cronie yakuake xorg-server xorg-drivers xorg-apps dri2proto plasma sddm kdeadmin kdebase kdegraphics kdemultimedia kdesdk kdeutils
-arch-chroot $mounted_root_path systemctl enable sddm
+arch-chroot $root_mountpoint pacman -S base-devel abs htop bash-completion vim terminus-font git cronie yakuake xorg-server xorg-drivers xorg-apps dri2proto plasma sddm kdeadmin kdebase kdegraphics kdemultimedia kdesdk kdeutils
+arch-chroot $root_mountpoint systemctl enable sddm
 
 read -p "Is this Arch Linux installation done inside Virtualbox ? [yes/no]: " virtualbox
 if [ "$virtualbox" = "yes" ]; then
-    arch-chroot $mounted_root_path pacman -S virtualbox-guest-utils
-    arch-chroot $mounted_root_path systemctl enable vboxservice.service
-    arch-chroot $mounted_root_path gpasswd -a $username vboxsf
+    arch-chroot $root_mountpoint pacman -S virtualbox-guest-utils
+    arch-chroot $root_mountpoint systemctl enable vboxservice.service
+    arch-chroot $root_mountpoint gpasswd -a $username vboxsf
 fi
 
-umount -R $mounted_root_path
-if [ "$rootfs_type" = "btrfs" ]; then
+if [ "$root_fstype" = "btrfs" ]; then
+    #### btrfs hook to initramfs
+    arch-chroot $root_mountpoint sed -i "s|MODULES=\"\"|MODULES=\"crc32c\"|g" /etc/mkinitcpio.conf
+    arch-chroot $root_mountpoint sed -i "s| fsck\"| fsck btrfs\"|g"           /etc/mkinitcpio.conf
+fi
+
+if [ "$root_crypt" = "yes" ]; then
+    echo "[INFO] add 'encrypt' to HOOKS in /etc/mkinitcpio.conf"
+    arch-chroot $root_mountpoint
+fi
+
+arch-chroot $root_mountpoint mkinitcpio -p linux
+
+umount -R $root_mountpoint
+if [ "$root_fstype" = "btrfs" ]; then
     umount -R /mnt/btrfs-root
 fi
 reboot
